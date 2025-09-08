572dcb9771618384fbdbb97f931e6b94ef655db1
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2025-09-08 10:47:19 -0400

    created main variables

---
+++ main.c
@@ -0,0 +1,13 @@
+# include <stdio.h >
+
+int main() {
+
+    char item[50] = "";
+    float price = 0.0f; 
+    int quantity = 0;
+    char currency = '$';
+    float total = 0.0f;
+
+
+    return 0;
+}
